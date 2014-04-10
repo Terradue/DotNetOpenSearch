64de783e388eb6d8758581940ded708319c22d98
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-04-10 09:25:34 +0200

    Assembly properties

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+/*                                                            
+* Copyright (C) 2010-2014 Terradue S.r.l.
+* 
+* This file is part of Terradue.OpenSearch.
+* 
+* Foobar is free software: you can redistribute it and/or modify it under the terms of the 
+* GNU General Public License as published by the Free Software Foundation, 
+* either version 3 of the License, or (at your option) any later version.
+* 
+* Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with Terradue.OpenSearch.
+* If not, see http://www.gnu.org/licenses/.
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle ("Terradue.OpenSearch")]
+[assembly: AssemblyDescription ("Terradue .Net OpenSearch Library")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Terradue")]
+[assembly: AssemblyProduct ("Terradue.OpenSearch")]
+[assembly: AssemblyCopyright ("Terradue")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.4.0.*")]
+[assembly: AssemblyInformationalVersion ("1.4.0.0")]
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
