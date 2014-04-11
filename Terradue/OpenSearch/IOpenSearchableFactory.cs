1482871ddb5b2309a405803f9a5b7780b0b95931
Emmanuel Mathot <emmanuel.mathot@skynet.be>
2014-04-11 08:13:14 +0200

    move to Github

---
+++ Terradue/OpenSearch/IOpenSearchableFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Terradue.OpenSearch {
+
+    /// <summary>
+    /// Represents a factory able to create IOpenSearchable object
+    /// </summary>
+    public interface IOpenSearchableFactory {
+
+        /// <summary>
+        /// Create an IOpenSearchable from an OpenSearchUrl
+        /// </summary>
+        /// <param name="url">URL to either a search or a description</param>
+        IOpenSearchable Create(OpenSearchUrl url);
+
+        /// <summary>
+        /// Create an IOpenSearchable from an OpenSearchDescription
+        /// </summary>
+        /// <param name="osd">OPenSearchDescription</param>
+        IOpenSearchable Create(OpenSearchDescription osd);
+
+    }
+}
+
---
+++ Terradue/OpenSearch/IProxiedOpenSearchable.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+
+namespace Terradue.OpenSearch {
+
+	/// \ingroup modules_openSearchEngine
+    public partial interface IProxiedOpenSearchable : IOpenSearchable {
+
+	}
+        
+}
+
