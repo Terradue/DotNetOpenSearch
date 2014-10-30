522fd714d820b3c3ea911bb9f9bd41d5249c1ce1
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-10-30 09:18:25 +0100

    ready to pack 1.7.3
    Signed-off-by: Emmanuel Mathot <emmanuel.mahot@gmail.com>

--- Terradue.OpenSearch/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright ("Terradue")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-[assembly: AssemblyVersion ("1.7.2.*")]
-[assembly: AssemblyInformationalVersion ("1.7.2")]
+[assembly: AssemblyVersion ("1.7.3.*")]
+[assembly: AssemblyInformationalVersion ("1.7.3")]
 
