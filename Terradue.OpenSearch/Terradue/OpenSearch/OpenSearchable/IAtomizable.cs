0280a5861c0f98df301d2ecb506b37bfcbbfeb4d
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-01-27 10:18:21 +0100

    GetOpenSearchParameters in IAtomizable
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenSearch/Terradue/OpenSearch/OpenSearchable/IAtomizable.cs
+++ Terradue.OpenSearch/Terradue/OpenSearch/OpenSearchable/IAtomizable.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿//
+//  IAtomizable.cs
+//
+//  Author:
+//       Emmanuel Mathot <emmanuel.mathot@terradue.com>
+//
+//  Copyright (c) 2014 Terradue
+
+using System;
 using Terradue.OpenSearch.Result;
 using System.Collections.Specialized;
 
 namespace Terradue.OpenSearch {
+
+    /// <summary>
+    /// Interface to implement a class as an item in a generic or heterogeneous OpenSearchable entity.
+    /// </summary>
     public interface IAtomizable {
 
+        /// <summary>
+        /// Transform the objec in its Atom item representation.
+        /// </summary>
+        /// <returns>The atom item. null if does not correspond to the parameters</returns>
+        /// <param name="parameters">Parameters to filter the items the search</param>
         AtomItem ToAtomItem(NameValueCollection parameters);
+
+        /// <summary>
+        /// Get the OpenSearch parameters applicable to the item.
+        /// </summary>
+        /// It should be ued by the generic OpenSearchable entity that use the object implementing IAtomizable
+        /// to build all the OpenSearch parameters of the search template. Due to the atom nature
+        /// of the item, the content type is not a parameter and should be considered as "application/atom+xml"
+        /// <returns>The open search parameters.</returns>
+        NameValueCollection GetOpenSearchParameters();
 
     }
 }
