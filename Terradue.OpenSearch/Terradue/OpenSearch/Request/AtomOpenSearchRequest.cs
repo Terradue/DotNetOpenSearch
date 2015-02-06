707dbcc198f299b79eec26e1c208d4d83f6d2cdf
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-02-06 11:15:15 +0100

    AtomOpenSearchRequest
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

---
+++ Terradue.OpenSearch/Terradue/OpenSearch/Request/AtomOpenSearchRequest.cs
@@ -0,0 +1,60 @@
+//
+//  FileOpenSearchRequest.cs
+//
+//  Author:
+//       Emmanuel Mathot <emmanuel.mathot@terradue.com>
+//
+//  Copyright (c) 2014 Terradue
+
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Web;
+using Terradue.OpenSearch.Response;
+using Terradue.OpenSearch.Result;
+using System.Diagnostics;
+
+namespace Terradue.OpenSearch.Request
+{
+    /// <summary>
+    /// OpenSearchRequest in memory that doesnot actually performs a request but is a container
+    /// for the response
+    /// </summary>
+    public class AtomOpenSearchRequest : OpenSearchRequest
+	{
+
+		NameValueCollection parameters;
+        Func<NameValueCollection, AtomFeed> feedGenerator;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Terradue.OpenSearch.Request.MemoryOpenSearchRequest"/> class.
+        /// </summary>
+        /// <param name="url">URL.</param>
+        /// <param name="contentType">Content type.</param>
+        public AtomOpenSearchRequest(OpenSearchUrl url, Func<NameValueCollection, AtomFeed> FeedGenerator) : base(url){
+            this.feedGenerator = FeedGenerator;
+            this.parameters = HttpUtility.ParseQueryString(url.Query);
+		}
+
+		#region implemented abstract members of OpenSearchRequest
+
+		public override IOpenSearchResponse GetResponse() {
+            Stopwatch sw = Stopwatch.StartNew();
+            var feed = feedGenerator(parameters);
+            sw.Stop();
+            return new AtomOpenSearchResponse(feed, sw.Elapsed);
+		}
+
+        public override OpenSearchUrl OpenSearchUrl {
+            get {
+                return base.OpenSearchUrl;
+            }
+        }
+
+		#endregion
+
+	}
+}
+
+
