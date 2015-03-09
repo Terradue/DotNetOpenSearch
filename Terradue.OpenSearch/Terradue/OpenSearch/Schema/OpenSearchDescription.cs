af54125230d39ece922328a91c7f73c0b145cdc1
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-03-09 17:45:00 +0100

    xmlns attributes
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenSearch/Terradue/OpenSearch/Schema/OpenSearchDescription.cs
+++ Terradue.OpenSearch/Terradue/OpenSearch/Schema/OpenSearchDescription.cs
@@ -8,426 +8,441 @@
 
 namespace Terradue.OpenSearch.Schema {
 
-	using System.Runtime.Serialization;
+    using System.Runtime.Serialization;
 
-	using System.Collections.ObjectModel;
+    using System.Collections.ObjectModel;
 
-	using System.Xml.Serialization;
+    using System.Xml.Serialization;
 
-	using System.Xml;
+    using System.Xml;
 
-	using System;
+    using System;
 
-	/// <remarks/>
-	[DataContract(Name="OpenSearchDescription",Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	[SerializableAttribute()]
-	[XmlTypeAttribute(Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	[XmlRootAttribute(Namespace="http://a9.com/-/spec/opensearch/1.1/", IsNullable=false)]
+    /// <remarks/>
+    [SerializableAttribute()]
+    [XmlTypeAttribute(Namespace = "http://a9.com/-/spec/opensearch/1.1/")]
+    [XmlRootAttribute(Namespace = "http://a9.com/-/spec/opensearch/1.1/", IsNullable = false)]
     public partial class OpenSearchDescription {
+
+        const int ShortNameMaxLength = 5;
 		
-		private string shortNameField;
+        private string shortNameField;
 		
-		private string descriptionField;
+        private string descriptionField;
 		
-		private string tagsField;
+        private string tagsField;
 		
-		private string contactField;
+        private string contactField;
 		
-		private OpenSearchDescriptionUrl[] urlField;
+        private OpenSearchDescriptionUrl[] urlField;
 		
-		private string longNameField;
+        private string longNameField;
 		
-		private OpenSearchDescriptionImage[] imageField;
+        private OpenSearchDescriptionImage[] imageField;
 		
-		private OpenSearchDescriptionQuery queryField;
+        private OpenSearchDescriptionQuery queryField;
 		
-		private string developerField;
+        private string developerField;
 		
-		private string attributionField;
+        private string attributionField;
 		
-		private string syndicationRightField;
+        private string syndicationRightField;
 		
-		private string adultContentField;
+        private string adultContentField;
 		
-		private string languageField;
+        private string languageField;
 		
-		private string outputEncodingField;
+        private string outputEncodingField;
 		
-		private string inputEncodingField;
-		
-		/// <remarks/>
+        private string inputEncodingField;
+
+        /// <remarks/>
         [DataMember]
-		public string ShortName {
-			get {
-				return this.shortNameField;
-			}
-			set {
-				this.shortNameField = value;
-			}
-		}
-		
-		/// <remarks/>
+        public string ShortName {
+            get {
+                return this.shortNameField;
+            }
+            set {
+
+                if (value.Length > ShortNameMaxLength)
+                    this.shortNameField = value.Substring(0, ShortNameMaxLength);
+                else
+                    this.shortNameField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
-		public string Description {
-			get {
-				return this.descriptionField;
-			}
-			set {
-				this.descriptionField = value;
-			}
-		}
-		
-		/// <remarks/>
+        public string Description {
+            get {
+                return this.descriptionField;
+            }
+            set {
+                this.descriptionField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string Tags {
-			get {
-				return this.tagsField;
-			}
-			set {
-				this.tagsField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.tagsField;
+            }
+            set {
+                this.tagsField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string Contact {
-			get {
-				return this.contactField;
-			}
-			set {
-				this.contactField = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlElementAttribute("Url")]
+            get {
+                return this.contactField;
+            }
+            set {
+                this.contactField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlElementAttribute("Url")]
         [DataMember]
         public OpenSearchDescriptionUrl[] Url {
-			get {
-				return this.urlField;
-			}
-			set {
-				this.urlField = value;
-			}
-        }   
-		
-		/// <remarks/>
+            get {
+                return this.urlField;
+            }
+            set {
+                this.urlField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string LongName {
-			get {
-				return this.longNameField;
-			}
-			set {
-				this.longNameField = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlElementAttribute("Image")]
+            get {
+                return this.longNameField;
+            }
+            set {
+                this.longNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlElementAttribute("Image")]
         [DataMember]
         public OpenSearchDescriptionImage[] Image {
-			get {
-				return this.imageField;
-			}
-			set {
-				this.imageField = value;
-			}
-		}
-		
-		/// <remarks/> 
+            get {
+                return this.imageField;
+            }
+            set {
+                this.imageField = value;
+            }
+        }
+
+        /// <remarks/> 
         [DataMember]
         public OpenSearchDescriptionQuery Query {
-			get {
-				return this.queryField;
-			}
-			set {
-				this.queryField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.queryField;
+            }
+            set {
+                this.queryField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string Developer {
-			get {
-				return this.developerField;
-			}
-			set {
-				this.developerField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.developerField;
+            }
+            set {
+                this.developerField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string Attribution {
-			get {
-				return this.attributionField;
-			}
-			set {
-				this.attributionField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.attributionField;
+            }
+            set {
+                this.attributionField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string SyndicationRight {
-			get {
-				return this.syndicationRightField;
-			}
-			set {
-				this.syndicationRightField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.syndicationRightField;
+            }
+            set {
+                this.syndicationRightField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string AdultContent {
-			get {
-				return this.adultContentField;
-			}
-			set {
-				this.adultContentField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.adultContentField;
+            }
+            set {
+                this.adultContentField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string Language {
-			get {
-				return this.languageField;
-			}
-			set {
-				this.languageField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.languageField;
+            }
+            set {
+                this.languageField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string OutputEncoding {
-			get {
-				return this.outputEncodingField;
-			}
-			set {
-				this.outputEncodingField = value;
-			}
-		}
-		
-		/// <remarks/>
+            get {
+                return this.outputEncodingField;
+            }
+            set {
+                this.outputEncodingField = value;
+            }
+        }
+
+        /// <remarks/>
         [DataMember]
         public string InputEncoding {
-			get {
-				return this.inputEncodingField;
-			}
-			set {
-				this.inputEncodingField = value;
-			}
-		}
+            get {
+                return this.inputEncodingField;
+            }
+            set {
+                this.inputEncodingField = value;
+            }
+        }
+
+        XmlSerializerNamespaces extraNamespace = new XmlSerializerNamespaces();
+        [XmlNamespaceDeclarations]
+        public XmlSerializerNamespaces ExtraNamespace {
+            get {
+                return extraNamespace;
+            }
+            set {
+                extraNamespace = value;
+            }
+        }
 
 
+        public OpenSearchDescriptionUrl GetUrlByType(string type) {
+            foreach (OpenSearchDescriptionUrl url in urlField) {
+                if (url.Type == type)
+                    return url;
+            }
 
-		public OpenSearchDescriptionUrl GetUrlByType (string type)
-		{
-			foreach (OpenSearchDescriptionUrl url in urlField) {
-				if ( url.Type == type )
-					return url;
-			}
+            return null;
+        }
+    }
 
-			return null;
-		}
-	}
-	
-	/// <remarks/>
-	[DataContract(Name="Url",Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	[SerializableAttribute()]
-	[XmlTypeAttribute(Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	public partial class OpenSearchDescriptionUrl {
+    /// <remarks/>
+    [DataContract(Name = "Url", Namespace = "http://a9.com/-/spec/opensearch/1.1/")]
+    [SerializableAttribute()]
+    [XmlTypeAttribute(Namespace = "http://a9.com/-/spec/opensearch/1.1/")]
+    public partial class OpenSearchDescriptionUrl {
 		
-		private string typeField;
+        private string typeField;
 		
-		private string templateField;
+        private string templateField;
 		
-		private string relField;
+        private string relField;
 
-		private int pageOffset = 1;
+        private int pageOffset = 1;
 		
-		private int indexOffset = 1;
+        private int indexOffset = 1;
 
-		public OpenSearchDescriptionUrl () {}
+        public OpenSearchDescriptionUrl() {
+        }
 
-		public OpenSearchDescriptionUrl ( string type, string template, string rel ){
-			this.Relation = rel;
-			this.Template = template;
-			this.Type = type;
-		}
-		
-		/// <remarks/>
-		[XmlAttribute(AttributeName="type")]
+        public OpenSearchDescriptionUrl(string type, string template, string rel) {
+            this.Relation = rel;
+            this.Template = template;
+            this.Type = type;
+        }
+
+        /// <remarks/>
+        [XmlAttribute(AttributeName = "type")]
         [DataMember]
         public string Type {
-			get {
-				return this.typeField;
-			}
-			set {
-				this.typeField = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlAttribute(AttributeName="template")]
+            get {
+                return this.typeField;
+            }
+            set {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttribute(AttributeName = "template")]
         [DataMember]
         public string Template {
-			get {
-				return this.templateField;
-			}
-			set {
-				this.templateField = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlAttribute(AttributeName="rel")]
+            get {
+                return this.templateField;
+            }
+            set {
+                this.templateField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttribute(AttributeName = "rel")]
         [DataMember]
         public string Relation {
-			get {
-				return this.relField;
-			}
-			set {
-				this.relField = value;
-			}
-		}
+            get {
+                return this.relField;
+            }
+            set {
+                this.relField = value;
+            }
+        }
 
-		[XmlAttribute(AttributeName="pageOffset")]
+        [XmlAttribute(AttributeName = "pageOffset")]
         [DataMember]
         public int PageOffset {
-			get {
-				return pageOffset;
-			}
-			set {
-				pageOffset = value;
-			}
-		}
+            get {
+                return pageOffset;
+            }
+            set {
+                pageOffset = value;
+            }
+        }
 
-		[XmlAttribute(AttributeName="indexOffset")]
+        [XmlAttribute(AttributeName = "indexOffset")]
         [DataMember]
         public int IndexOffset {
-			get {
-				return indexOffset;
-			}
-			set {
-				indexOffset = value;
-			}
-		}
-	}
-	
-	/// <remarks/>
-	[DataContract(Name="Image",Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	[SerializableAttribute()]
-	[XmlTypeAttribute(Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	public partial class OpenSearchDescriptionImage {
+            get {
+                return indexOffset;
+            }
+            set {
+                indexOffset = value;
+            }
+        }
+    }
 
-		private int heightField;
+    /// <remarks/>
+    [DataContract(Name = "Image", Namespace = "http://a9.com/-/spec/opensearch/1.1/")]
+    [SerializableAttribute()]
+    [XmlTypeAttribute(Namespace = "http://a9.com/-/spec/opensearch/1.1/")]
+    public partial class OpenSearchDescriptionImage {
 
-		private int widthField;
+        private int heightField;
+
+        private int widthField;
 		
-		private string typeField1;
+        private string typeField1;
 		
-		private string valueField1;
-		
-		/// <remarks/>
-		[XmlAttribute]
+        private string valueField1;
+
+        /// <remarks/>
+        [XmlAttribute]
         [DataMember]
         public int height {
-			get {
-				return this.heightField;
-			}
-			set {
-				this.heightField = value;
-			}
-		}
+            get {
+                return this.heightField;
+            }
+            set {
+                this.heightField = value;
+            }
+        }
 
-		/// <remarks/>
-		[XmlAttribute]
+        /// <remarks/>
+        [XmlAttribute]
         [DataMember]
         public int width {
-			get {
-				return this.widthField;
-			}
-			set {
-				this.widthField = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlAttribute]
+            get {
+                return this.widthField;
+            }
+            set {
+                this.widthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttribute]
         [DataMember]
         public string type {
-			get {
-				return this.typeField1;
-			}
-			set {
-				this.typeField1 = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlText]
+            get {
+                return this.typeField1;
+            }
+            set {
+                this.typeField1 = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlText]
         [DataMember]
         public string Value {
-			get {
-				return this.valueField1;
-			}
-			set {
-				this.valueField1 = value;
-			}
-		}
-	}
-	
-	/// <remarks/>
-	[DataContract(Name="Query",Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	[SerializableAttribute()]
-	[XmlTypeAttribute(Namespace="http://a9.com/-/spec/opensearch/1.1/")]
-	public partial class OpenSearchDescriptionQuery {
+            get {
+                return this.valueField1;
+            }
+            set {
+                this.valueField1 = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [DataContract(Name = "Query", Namespace = "http://a9.com/-/spec/opensearch/1.1/")]
+    [SerializableAttribute()]
+    [XmlTypeAttribute(Namespace = "http://a9.com/-/spec/opensearch/1.1/")]
+    public partial class OpenSearchDescriptionQuery {
 		
-		private string roleField;
+        private string roleField;
 		
-		private string searchTermsField;
+        private string searchTermsField;
 		
-		private string valueField2;
-		
-		/// <remarks/>
-		[XmlAttribute]
+        private string valueField2;
+
+        /// <remarks/>
+        [XmlAttribute]
         [DataMember]
         public string role {
-			get {
-				return this.roleField;
-			}
-			set {
-				this.roleField = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlAttribute]
+            get {
+                return this.roleField;
+            }
+            set {
+                this.roleField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttribute]
         [DataMember]
         public string searchTerms {
-			get {
-				return this.searchTermsField;
-			}
-			set {
-				this.searchTermsField = value;
-			}
-		}
-		
-		/// <remarks/>
-		[XmlText]
+            get {
+                return this.searchTermsField;
+            }
+            set {
+                this.searchTermsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlText]
         [DataMember]
         public string Value {
-			get {
-				return this.valueField2;
-			}
-			set {
-				this.valueField2 = value;
-			}
-		}
-	}
+            get {
+                return this.valueField2;
+            }
+            set {
+                this.valueField2 = value;
+            }
+        }
+    }
    
 }
