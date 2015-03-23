2dd77c09e72ec1033243cf3d4c91d0b6ed0a66c0
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-03-23 15:31:15 +0100

    d
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenSearch/Terradue/OpenSearch/Response/HttpOpenSearchResponse.cs
+++ Terradue.OpenSearch/Terradue/OpenSearch/Response/HttpOpenSearchResponse.cs
@@ -60,7 +60,7 @@ namespace Terradue.OpenSearch.Response {
         }
 
         public override IOpenSearchResponse CloneForCache() {
-            return new MemoryOpenSearchResponse(payload, ContentType);
+            return new MemoryOpenSearchResponse(payload.Clone(), ContentType);
         }
 
         #endregion
