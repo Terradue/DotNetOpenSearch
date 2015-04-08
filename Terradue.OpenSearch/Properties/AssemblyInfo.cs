06bf3b084c5b011d4604b1f71be187f4407ae67f
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-04-08 15:40:55 +0200

    ready to pack 1.9.10
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenSearch/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright ("Terradue")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-[assembly: AssemblyVersion ("1.9.9.*")]
-[assembly: AssemblyInformationalVersion ("1.9.9")]
+[assembly: AssemblyVersion ("1.9.10.*")]
+[assembly: AssemblyInformationalVersion ("1.9.10")]
