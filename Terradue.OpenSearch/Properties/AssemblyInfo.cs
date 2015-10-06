cbe528b199871845f97c9009b45f9fe0a4dfa734
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-10-06 16:49:17 +0200

    ready to pack 1.11.11
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenSearch/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@
 @{
     DotNetOpenSearch Software Package
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.11.10
+    \xrefitem sw_version "Versions" "Software Package Version" 1.11.11
 
     \xrefitem sw_link "Link" "Software Package Link" [DotNetOpenSearch](https://github.com/Terradue/DotNetOpenSearch)
 
@@ -42,8 +42,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright ("Terradue")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-[assembly: AssemblyVersion ("1.11.10.*")]
-[assembly: AssemblyInformationalVersion ("1.11.10")]
+[assembly: AssemblyVersion ("1.11.11.*")]
+[assembly: AssemblyInformationalVersion ("1.11.11")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile="log4net.config", Watch = true)]
 
