dde3be63d24df499697822b0b712daadf88e8732
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-10-06 15:01:57 +0200

    new comparer at resut item level
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

---
+++ Terradue.OpenSearch/Terradue/OpenSearch/Result/OpenSearchResultItemComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Terradue.OpenSearch.Result {
+    public class OpenSearchResultItemComparer : IEqualityComparer<IOpenSearchResultItem> {
+        #region IEqualityComparer implementation
+
+        public bool Equals(IOpenSearchResultItem x, IOpenSearchResultItem y) {
+            if (x.Identifier == y.Identifier) return true;
+            else return false;
+        }
+
+        public int GetHashCode(IOpenSearchResultItem obj) {
+            return obj.Identifier.GetHashCode();
+        }
+
+        #endregion
+    }
+}
+
