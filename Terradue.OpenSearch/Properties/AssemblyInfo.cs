4cb7d3cd6640ccedf421a3207288de7221292570
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2017-12-04 08:08:50 +0100

    bump to 1.15.6
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

--- Terradue.OpenSearch/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@
 @{
     Terradue.OpenSearch is a .Net library that provides an engine perform OpenSearch query from a class or an URL to multiple and custom types of results
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.15.5
+    \xrefitem sw_version "Versions" "Software Package Version" 1.15.6
 
     \xrefitem sw_link "Link" "Software Package Link" [DotNetOpenSearch](https://github.com/Terradue/DotNetOpenSearch)
 
@@ -49,6 +49,6 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNetOpenSearch/blob/master/LICENSE")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.15.5.*")]
-[assembly: AssemblyInformationalVersion("1.15.5")]
+[assembly: AssemblyVersion("1.15.6.*")]
+[assembly: AssemblyInformationalVersion("1.15.6")]
 
