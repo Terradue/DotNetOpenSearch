8ea5465630aa62ffc7b1a79d909b784f4d649cd5
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2019-07-26 15:20:43 +0200

    username with @

--- Terradue.OpenSearch.Test/AuthOpenSearchable.Test.cs
+++ Terradue.OpenSearch.Test/AuthOpenSearchable.Test.cs
@@ -7,10 +7,12 @@ using System.Linq;
 using log4net.Config;
 using System.IO;
 
-namespace Terradue.OpenSearch.Test {
+namespace Terradue.OpenSearch.Test
+{
 
     [TestFixture]
-    public class AuthOpenSearchableTest {
+    public class AuthOpenSearchableTest
+    {
 
         [SetUp]
         public void RunBeforeTests()
@@ -28,7 +30,7 @@ namespace Terradue.OpenSearch.Test {
 
             var settings = new OpenSearchableFactorySettings(ose);
             settings.Credentials = new System.Net.NetworkCredential("demo1", "Demo199++");
-			UrlBasedOpenSearchableFactory factory = new UrlBasedOpenSearchableFactory(settings);
+            UrlBasedOpenSearchableFactory factory = new UrlBasedOpenSearchableFactory(settings);
 
             IOpenSearchable entity1 = factory.Create(new OpenSearchUrl("https://catalog.terradue.com/demo1/search"));
 
@@ -42,6 +44,48 @@ namespace Terradue.OpenSearch.Test {
             osr = ose.Query(entity1, nvc, "atom");
 
             OpenSearchFactory.FindOpenSearchable(settings, new Uri("https://catalog.terradue.com/demo1/search"), null);
+
+
+
+        }
+
+
+            OpenSearchEngine ose = new OpenSearchEngine();
+            ose.LoadPlugins();
+
+            var settings = new OpenSearchableFactorySettings(ose);
+            settings.Credentials = new System.Net.NetworkCredential("demo1", "Demo199++");
+            UrlBasedOpenSearchableFactory factory = new UrlBasedOpenSearchableFactory(settings);
+
+            IOpenSearchable entity1 = factory.Create(new OpenSearchUrl("https://catalog.terradue.com/demo1/search"));
+
+            NameValueCollection nvc = new NameValueCollection();
+
+            var osr = ose.Query(entity1, nvc, "atom");
+
+            nvc = new NameValueCollection();
+            nvc.Set("count", "10");
+
+            osr = ose.Query(entity1, nvc, "atom");
+
+            OpenSearchFactory.FindOpenSearchable(settings, new Uri("https://catalog.terradue.com/demo1/search"), null);
+
+
+
+        }
+
+        [Test]
+        public void SpecialCharCredentialHttp()
+        {
+
+            OpenSearchEngine ose = new OpenSearchEngine();
+            ose.LoadPlugins();
+
+            var settings = new OpenSearchableFactorySettings(ose);
+            settings.Credentials = new System.Net.NetworkCredential("eod.exp@gmail.com", "fred1960");
+            UrlBasedOpenSearchableFactory factory = new UrlBasedOpenSearchableFactory(settings);
+
+            OpenSearchFactory.FindOpenSearchable(settings, new Uri("https://finder.creodias.eu/resto/api/collections/describe.xml"), null);
 
 
 
