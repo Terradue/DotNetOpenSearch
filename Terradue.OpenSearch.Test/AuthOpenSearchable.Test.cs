945761ed878418322a117ae087d53845b784faf6
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2019-08-08 16:00:31 +0200

    fix merge

--- Terradue.OpenSearch.Test/AuthOpenSearchable.Test.cs
+++ Terradue.OpenSearch.Test/AuthOpenSearchable.Test.cs
@@ -88,37 +88,7 @@ namespace Terradue.OpenSearch.Test
 
             OpenSearchFactory.FindOpenSearchable(settings, new Uri("https://catalog.terradue.com/demo1/search"), null);
 
-
-
         }
-
-        public void NetworkCredentialRedirectedHttp()
-        {
-
-            OpenSearchEngine ose = new OpenSearchEngine();
-            ose.LoadPlugins();
-
-            var settings = new OpenSearchableFactorySettings(ose);
-            settings.Credentials = new System.Net.NetworkCredential("demo1", "Demo199++");
-			UrlBasedOpenSearchableFactory factory = new UrlBasedOpenSearchableFactory(settings);
-
-            IOpenSearchable entity1 = factory.Create(new OpenSearchUrl("https://catalog.terradue.com/demo1/search"));
-
-            NameValueCollection nvc = new NameValueCollection();
-
-            var osr = ose.Query(entity1, nvc, "atom");
-
-            nvc = new NameValueCollection();
-            nvc.Set("count", "10");
-
-            osr = ose.Query(entity1, nvc, "atom");
-
-            OpenSearchFactory.FindOpenSearchable(settings, new Uri("https://catalog.terradue.com/demo1/search"), null);
-
-
-
-        }
-
 
     }
 }
